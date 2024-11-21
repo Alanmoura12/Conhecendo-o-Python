48aa69c9aa91fd1b19c29b2f30a7a98b0590802c
Alan Moura Santana <alan.moura2@hotmail.com>
2024-11-21 09:20:26 -0300

    tipos de str int e float

---
+++ convertendo_tipos.py
@@ -0,0 +1,12 @@
+print (int (1.0564894))
+print (int("10"))
+print (float("10.45"))
+print (float(100))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print (100 / 2)
+print (100 // 2) #retorna o valor inteiro (int) (não divide em duas vezes)
