5dbc82ac9a3af4dcf70a49d576d0a7d765adc317
Alan Moura Santana <alan.moura2@hotmail.com>
2024-11-21 09:31:41 -0300

    boas praticas

---
+++ variaveis_contantes.py
@@ -0,0 +1,12 @@
+nome = "Pedro"
+idade = 32
+
+nome = "Paula"
+
+print (nome, idade)
+
+limite_saque_diario = 1000 # boas praticas snake case (modo de escrever a variavel e bem sugestivo sem encurtar)
+
+BRASILIN_STATES = [ "SP", "RJ", "SC", "RS"] #variavel constante tem que ser toda em maiusculo (Boas praticas)
+
+print (BRASILIN_STATES)
